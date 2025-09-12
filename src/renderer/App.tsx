14e13aaf1763c5c0723d794efcbe7f5de029e345
Simon-Lage <150927092+Simon-Lage@users.noreply.github.com>
2025-09-12 10:19:33 +0200

    chore: prettier fix

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,7 +2,6 @@ import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './tailwind.css';
 import './App.css';
 
-
 export default function App() {
   return (
     <Router>
