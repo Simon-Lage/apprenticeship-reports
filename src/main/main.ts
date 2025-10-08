af4e21606122a77571cdc0f4fd094f23cbf0d213
Simon-Lage <150927092+Simon-Lage@users.noreply.github.com>
2025-10-08 16:11:55 +0200

    bugfix: rewrote utils

--- src/main/main.ts
+++ src/main/main.ts
@@ -14,7 +14,7 @@ import { app, BrowserWindow, shell, ipcMain, dialog } from 'electron';
 import { autoUpdater } from 'electron-updater';
 import log from 'electron-log';
 import MenuBuilder from './menu';
-import { resolveHtmlPath } from './util';
+import { resolveHtmlPath } from './utils';
 
 class AppUpdater {
   constructor(mainWindow: BrowserWindow) {
