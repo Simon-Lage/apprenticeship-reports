dfccc4a9f80d8f0876f953b2b4eafa1eb20911c8
Simon-Lage <150927092+Simon-Lage@users.noreply.github.com>
2025-10-08 16:20:16 +0200

    test update

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<div>test-update 4</div>} />
+        <Route path="/" element={<div>test-update 4.1</div>} />
       </Routes>
     </Router>
   );
